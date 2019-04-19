4031d3ee8ac93804bf82c0c0bd90dd8b65b17547
menglei <meng.lei@data-u.com>
2019-04-19 12:18:01 +0800

    first commit

---
+++ src/main/java/com/datau/springboot07datamybatis/SpringBoot07DataMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.datau.springboot07datamybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot07DataMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot07DataMybatisApplication.class, args);
+    }
+
+}
